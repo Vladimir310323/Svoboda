df8d8e67109e71df8134dfbb458a32ee412df9b1
Vladimir310323 <Vladimir.Babenko.1703@gmail.com>
2026-07-23 17:26:10 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'svoboda-tracker-v2';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first for everything: always try to get the latest version when online,
+// only fall back to the cached copy when there's no connection at all.
+// This is what makes updates show up immediately instead of getting stuck on an old cached version.
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    fetch(req)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, resClone));
+        return res;
+      })
+      .catch(() => caches.match(req))
+  );
+});
